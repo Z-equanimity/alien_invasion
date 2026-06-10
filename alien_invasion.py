9f5a456a208914cecf3bea70ac2b1fbb5ef3f988
Zzw <2865761465@qq.com>
2026-06-10 17:54:22 +0800

    创建setting类

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,14 @@
 import sys
 import pygame
+from settings import Settings
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
         self.clock = pygame.time.Clock()
-        self.bg_color = (230,230,230)
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
     def run_game(self):
         """开始游戏的主循环"""
         while True:
@@ -15,8 +16,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                    #每次循环都重新绘制屏幕
-                    self.screen.fill(self.bg_color)                
+            #每次循环都重新绘制屏幕
+            self.screen.fill(self.settings.bg_color)                
             # 让最近绘制的屏幕可见
             pygame.display.flip()
             # 控制游戏帧率为 60 FPS
