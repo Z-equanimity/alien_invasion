f75167588b0e7bfb2b740582eb2922baf23bab10
Zzw <2865761465@qq.com>
2026-06-11 11:10:47 +0800

    重构：_check_event()方法和_update_screen()方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,22 +11,33 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.settings = Settings()
         self.clock = pygame.time.Clock()        
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+             (self.settings.screen_width,self.settings.screen_height))
         self.ship = Ship(self)
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
+            self._check_events()
+            self._update_screen()
+            # 控制游戏帧率为 60 FPS
+            self.clock.tick(60)
+
+    def _check_events(self):   
+            """响应按键和鼠标事件"""
             # 侦听键盘和鼠标事件
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()
-            #每次循环都重新绘制屏幕
-            self.screen.fill(self.settings.bg_color)   
-            self.ship.blitme()             
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
-            # 控制游戏帧率为 60 FPS
-            self.clock.tick(60)
+                    sys.exit()            
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+         #每次循环都重新绘制屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
 if __name__ == "__main__":
     # 创建游戏实例并运行游戏
     ai = AlienInvasion()
