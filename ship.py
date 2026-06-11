ddfd41c0a28432406bdcaa4c331e9eebf8eee39c
Zzw <2865761465@qq.com>
2026-06-11 12:27:09 +0800

    限制飞船活动范围

--- ship.py
+++ ship.py
@@ -20,11 +20,12 @@ class Ship:
     def update(self):
         """根据移动标志调整飞船的位置"""
         # 更新飞船的属性x的值，而不是外接矩形的属性x的值
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
               self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect_left > 0:
               self.x -= self.settings.ship_speed
-              
+        # 根据self.x的值更新rect的值
+        self.rect.x = self.x
         
     def blitme(self):
         """在指定位置绘制飞船"""
