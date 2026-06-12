81b00aceea467b3088cb94e9bdf451b97e9eeec8
Zzw <2865761465@qq.com>
2026-06-12 14:37:21 +0800

    按q退出

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,7 +41,9 @@ class AlienInvasion:
                 self.ship.moving_right = True
             elif event.key == pygame.K_LEFT:
                  self.ship.moving_left = True
-                 
+            elif event.key == pygame.K_q:
+                 sys.exit()
+
     def _check_keyup_events(self,event):
             """响应释放"""                
             if  event.key == pygame.K_RIGHT:
