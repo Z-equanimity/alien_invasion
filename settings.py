b27378e96667f8fe8a2ff63b84365d03389f2191
Zzw <2865761465@qq.com>
2026-06-12 15:05:24 +0800

    添加子弹设置

--- settings.py
+++ settings.py
@@ -8,3 +8,8 @@ class Settings:
         self.screen_height = 675
         # 飞船的设置
         self.ship_speed = 1.5
+        # 子弹的设置
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_coler = (60,60,60)
