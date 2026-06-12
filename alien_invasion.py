02ae4d3f7d23eae33f410ca82088ab7ef897b90a
Zzw <2865761465@qq.com>
2026-06-12 16:11:22 +0800

    创建_uodate_bullets方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,51 +32,45 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            # 删除已消失的子弹
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                    # # 验证子弹确实被删除
-                    # print(len(self.bullets))
+            self._update_bullets()
             self._update_screen()
             # 控制游戏帧率为 60 FPS
             self.clock.tick(60)
 
     def _check_events(self):   
-            """响应按键和鼠标事件"""
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()                
-                elif event.type == pygame.KEYDOWN:
-                    self._check_keydown_events(event)
-                elif event.type == pygame.KEYUP:
-                    self._check_keyup_events(event)
+        """响应按键和鼠标事件"""
+        # 侦听键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()                
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
     def _check_keydown_events(self,event):
-            """响应按下"""
-            if event.key == pygame.K_RIGHT:
-                self.ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                 self.ship.moving_left = True
-            elif event.key == pygame.K_ESCAPE:
-                 sys.exit()
-            elif event.key == pygame.K_SPACE:
-                 self._fire_bullet()
+        """响应按下"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self,event):
-            """响应释放"""                
-            if  event.key == pygame.K_RIGHT:
-                self.ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                self.ship.moving_left = False
+        """响应释放"""                
+        if  event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     
     def _fire_bullet(self):
-         """创建一颗子弹，并将其加入编组bullets"""
-         if len(self.bullets) < self.settings.bullets_allowed:
-              new_bullet= Bullet(self)
-              self.bullets.add(new_bullet)
+        """创建一颗子弹，并将其加入编组bullets"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet= Bullet(self)
+            self.bullets.add(new_bullet)
  
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
@@ -87,6 +81,16 @@ class AlienInvasion:
         self.ship.blitme()
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+    def _update_bullets(self):
+        """更新子弹的位置并删除已经消失的子弹"""
+        # 更新子弹位置
+        self.bullets.update()
+        # 删除已消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+                # # 验证子弹确实被删除
+                # print(len(self.bullets))
 
 if __name__ == "__main__":
     # 创建游戏实例并运行游戏
