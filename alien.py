5a9e34f637af3b61ec9ec6747845bd1da382c381
Zzw <2865761465@qq.com>
2026-06-13 19:30:39 +0800

    修改super().__init__()传参错误

--- alien.py
+++ alien.py
@@ -5,7 +5,7 @@ class Alien(Sprite):
     """表示单个外星人的类"""
     def __init__(self, ai_game):
         """初始化外星人并设置其起始位置"""
-        super().__init__(ai_game)
+        super().__init__()
         self.screen = ai_game.screen
 
         # 加载外星人图像，并设置其rect的属性
