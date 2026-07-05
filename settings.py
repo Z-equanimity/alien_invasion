093019b2571af506b651704a8a0e220f2864115d
Zzw <2865761465@qq.com>
2026-07-05 17:26:43 +0800

    提高外星人得分速度

--- settings.py
+++ settings.py
@@ -18,6 +18,8 @@ class Settings:
         self.fleet_drop_speed = 10
         # 以什么速度加快游戏的节奏
         self.speedup_scale = 1.1
+        # 外星人分数提高的速度
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -35,3 +37,4 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points*self.score_scale)
