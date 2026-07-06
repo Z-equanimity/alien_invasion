9fabb0d86a6d23e7bac81ca7a99457c121c63431
Zzw <2865761465@qq.com>
2026-07-06 15:41:37 +0800

    显示余下飞船数量

--- alien_invasion.py
+++ alien_invasion.py
@@ -95,6 +95,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             # 清空外星人列表和子弹列表
@@ -220,6 +221,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # 将ship_lift减1
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # 清空外星人列表和子弹列表
             self.bullets.empty()
